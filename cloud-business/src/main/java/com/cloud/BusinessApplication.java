954766235d4e59a75ff86cf9ae412132317075ab
guofuqiang00 <gfqjava@163.com>
2020-11-07 20:43:54 +0800

    t1

--- cloud-business/src/main/java/com/cloud/BusinessApplication.java
+++ cloud-business/src/main/java/com/cloud/BusinessApplication.java
@@ -2,10 +2,12 @@ package com.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.cloud.dao")
+@EnableEurekaClient
 public class BusinessApplication {
     public static void main(String[] args) {
         SpringApplication.run(BusinessApplication.class,args);
